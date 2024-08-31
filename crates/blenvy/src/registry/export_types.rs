878b0417e750725d7bb6f2e33e3a61ec92dd7ef2
Marco Buono <thecoreh@gmail.com>
2024-08-30 21:34:17 -0300

    Update to work with latest bevy main

--- crates/blenvy/src/registry/export_types.rs
+++ crates/blenvy/src/registry/export_types.rs
@@ -173,6 +173,12 @@ pub fn export_type(reg: &TypeRegistration) -> (String, Value) {
             "valueType": json!({"type": typ(info.value_type_path_table().path())}),
             "keyType": json!({"type": typ(info.key_type_path_table().path())}),
         }),
+        TypeInfo::Set(info) => json!({
+            "long_name": t.type_path(),
+            "type": "array",
+            "typeInfo": "Set",
+            "valueType": json!({"type": typ(info.value_type_path_table().path())}),
+        }),
         TypeInfo::Tuple(info) => json!({
             "long_name": t.type_path(),
             "type": "array",
